7f66ce5daa63b09faf72aa8b8ee7441a75550688
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-13 22:56:12 +0600

    Box

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("OK");
+// jQuery(selector).function()
+
+
+
+
+jQuery(document).ready(function(){
+jQuery("#hide").click(function(){
+    jQuery(".box").hide(1000);
+});
+
+jQuery("#show").click(function(){
+    jQuery(".box").show(1000);
+});
+
+jQuery("#toggle").click(function(){
+    jQuery(".box").toggle(1000);
+    
+});
+
+
+});
